4bfac103038d2dbd950e49e9a31c0e410f0da9ed
Claudia Tona <ctona@Claudias-MacBook-Pro.local>
2025-01-22 20:01:52 -0800

    Clase 103-1

---
+++ scripts/script.js
@@ -0,0 +1,59 @@
+// console.log("Variable");
+// //let:
+// //var:
+// // keyword nameVar = value;
+// var firstName = "Gabriel";
+// var lastName = "Tona";
+
+// firstName = "Claudia";
+
+// let myAge = 30;
+// let myHeight = 5.7;
+
+// let isStudent = false;
+
+// //display values on cconsole
+// console.log("My name is " + firstName + " " + lastName);
+// console.log("My name is " + firstName + " " + lastName + ", and I am " + myAge + " years old, and my height is " + myHeight);
+
+// // Aritmetic operations
+
+// let num1 = 10;
+// let num2 = 2;
+
+
+// let sum = num1 + num2;
+// console.log("The sum is: " + sum);
+// console.log("The substraction is: " + (num1 - num2));
+// console.log("The multiplication is: " + (num1 * num2));
+// console.log("The division is: " + (num1 / num2));
+
+
+// //Challenge 1
+// const pi = 3.1416;
+// let radius = prompt("Enter the radio: ");
+
+// let circleArea = pi * radius * radius;
+// console.log("The area of a circle with a radius of " + radius + "cm is " + circleArea);
+
+// //Challenge 2
+// let integerNumber = 1625;
+// console.log("The value of integerNumber is: " + integerNumber);
+// integerNumber = 375;
+// alert(integerNumber + 125);
+
+//Challenge 3
+// let age = prompt("Enter your age: ");
+// console.log(age);
+// alert("Thank you for your answer.");
+// window.alert("You are " + age + " years old.");
+
+//Difference between variables and constants
+let aVariable;
+//assigning a value
+aVariable = 20; 
+
+//define and assign the value in the same line
+let anotherVariable = 22;
+
+console.log(aVariable,anotherVariable);
